edb27a590cf1815a32e553d27336e113489a4239
Kuuzzii <betlogpapamo123@gmail.com>
2025-05-18 01:58:49 +0800

    Update home.js

--- home.js
+++ home.js
@@ -1,4 +1,4 @@
-const API_KEY = 'YOUR_API_KEY_HERE'; // Replace with your actual API key
+const API_KEY = 'b139bc417606842811f1526ae92572bc'; // Replace with your actual API key
 const BASE_URL = 'https://api.themoviedb.org/3/';
 const IMG_URL = 'https://image.tmdb.org/t/p/w500'; // Use a specific image size like w500
 
